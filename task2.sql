2109a7a5c3c29c6fd9e11c4c4c02a977c729832e
Lokisonn <fenrir_lokisonn@mail.ru>
2025-12-11 15:22:24 +0300

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,62 +1,58 @@
 --Задание 2
 -- Название и продолжительность самого длительного трека
-select  name, duration from tracks
-where duration = (select MAX(duration) from tracks);
+select  name_tracks, duration from tracks1
+where duration = (select MAX(duration) from tracks1);
 
 --Название треков, продолжительность которых не менее 3,5 минут
-select name, duration from tracks
+select name_tracks, duration from tracks1
 where duration >= 210;
 
 --Названия сборников, вышедших в период с 2018 по 2020 год включительно
-select name, yearofrelease  from collections
+select name_collections, yearofrelease  from collections1
 where yearofrelease >= 2018 and yearofrelease <=2020;
 
 --Исполнители, чьё имя состоит из одного слова
-select name  from performers
-where name not like '% %';
+select name_performers  from performers1
+where name_performers not like '% %';
 
 --Название треков, которые содержат слово «мой» или «my»
-select name  from tracks
-where lower(name) like 'мой %' or lower(name) like '% мой' or lower(name) like '% мой %' or lower(name) like 'мой' 
-or lower(name) like 'my %' or lower(name) like '% my' or lower(name) like '% my %' or lower(name) like 'my';
+select name_tracks  from tracks1
+where lower(name_tracks) like 'мой %' or lower(name_tracks) like '% мой' or lower(name_tracks) like '% мой %' or lower(name_tracks) like 'мой' 
+or lower(name_tracks) like 'my %' or lower(name_tracks) like '% my' or lower(name_tracks) like '% my %' or lower(name_tracks) like 'my';
 
 --Задание 3
 --Количество исполнителей в каждом жанре 
-select g.name, count(gp.performer_id) as count_performers
-from genres g 
-left  join genre_performer gp  on g.id = gp.genre_id
-group by g.name
+select g.name_genre, count(gp.performer_id) as count_performers
+from Genres1 g 
+left  join Genre_performer gp  on g.id = gp.genre_id
+group by g.name_genre
 order by count_performers desc;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов
-select count(name) from tracks t 
+select count(name_tracks) from tracks1 t 
 join albums1 a on t.album_id = t.album_id 
 where a.YearOfRelease between 2019 and 2020;
 
 --Средняя продолжительность треков по каждому альбому
-select a.name, AVG(t.duration) as avg_duration
-from tracks t 
-left  join albums a  on a.id = t.album_id
-group by a.name;
+select a.name_albums, AVG(t.duration) as avg_duration
+from tracks1 t 
+left  join albums1 a  on a.id = t.album_id
+group by a.name_albums;
 
 --Все исполнители, которые выпустили альбомы НЕ в 2020 году.
-select name from performers p 
-where name not in (
-select name from albums a 
+select name_performers from performers1 p 
+where name_performers  not in (
+select name_albums from albums1 a 
 join album_performer ea on ea.album_id = a.id
-join performers p on ea.performer_id = p.id
+join performers1 p on ea.performer_id = p.id
 where a.yearofrelease = 2020);
 
 --Названия сборников, в которых присутствует конкретный исполнитель.
-select c.name as collection, t.name as name_track
-from collections c
+select c.name_collections as collection1, t.name_tracks as name_track
+from collections1 c
 join collection_track ct on c.id = ct.collection_id 
-join tracks t on t.id = ct.track_id 
-join albums a on a.id = t.album_id
+join tracks1 t on t.id = ct.track_id 
+join albums1 a on a.id = t.album_id
 join album_performer ap  on t.album_id = ap.album_id
-join performers p on p.id =ap.performer_id
-where p.name = 'Tina Turner';
-
-
-
-
+join performers1 p on p.id =ap.performer_id
+where p.name_performers = 'Tina Turner'
